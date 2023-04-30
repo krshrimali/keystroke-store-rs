8d75a59eccece8078198a06806d191cb43f1ec3d
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-04-30 11:37:44 +0530

    use rdev

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rdev::{listen, Event};
+
+fn callback(event: Event) {
+    // println!("My callback {:?}", event);
+    match event.event_type {
+        rdev::EventType::KeyPress(_) => {
+            println!("Found keypress event with key: {:?}", event.name.unwrap())
+        }
+        rdev::EventType::KeyRelease(_) => {
+            println!(
+                "Found KeyRelease event with key: {:?}",
+                event.name.unwrap_or(String::from(""))
+            )
+        }
+        _ => (),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = listen(callback) {
+        println!("Error: {:?}", error);
+    }
 }
