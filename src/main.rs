4edb14f8406627ce80e7135fc28b0524215cc310
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-04-30 16:07:17 +0530

    further handling of keyboard events

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use rdev::{listen, Event};
+use rdev::{listen, Event, EventType};
+
+fn handle_keyboard_events(event_type: EventType) {
+    // it's safe to assume that the event will always be either KeyPress or KeyRelease event, this is handled one level up (callback fn)
+    let key_context: Option<rdev::Key> = match event_type {
+        EventType::KeyPress(key) => Some(key),
+        EventType::KeyRelease(key) => Some(key),
+        _ => None,
+    };
+    println!("key context: {:?}", key_context.unwrap());
+}
 
 fn callback(event: Event) {
-    // println!("My callback {:?}", event);
     match event.event_type {
         rdev::EventType::KeyPress(_) => {
-            println!("Found keypress event with key: {:?}", event.name.unwrap())
+            handle_keyboard_events(event.event_type);
         }
         rdev::EventType::KeyRelease(_) => {
-            println!(
-                "Found KeyRelease event with key: {:?}",
-                event.name.unwrap_or(String::from(""))
-            )
+            handle_keyboard_events(event.event_type);
         }
         _ => (),
     }
