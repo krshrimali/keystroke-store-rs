6fd6511424763894445599b9f5324ab103836847
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-04-30 20:05:29 +0530

    Wow, kafka producer now; thanks @imsrbh

---
+++ src/kafka_producer.rs
@@ -0,0 +1,38 @@
+use kafka::error::Error as KafkaError;
+use kafka::producer::{Producer, Record, RequiredAcks};
+use rdev::Key;
+use std::time::Duration;
+
+pub fn send_to_kafka(msg: Key) {
+    // env_logger::init();
+    let broker = "localhost:9092";
+    let topic = "test";
+
+    let data = serde_json::to_string(&msg).unwrap();
+
+    if let Err(e) = produce_message(data.as_bytes(), topic, vec![broker.to_owned()]) {
+        println!("Failed producing messages: {:?}", e);
+    }
+    println!("SENT");
+}
+
+fn produce_message<'a, 'b>(
+    data: &'a [u8],
+    topic: &'b str,
+    brokers: Vec<String>,
+) -> Result<(), KafkaError> {
+    let mut producer = Producer::from_hosts(brokers)
+        .with_ack_timeout(Duration::from_secs(1))
+        .with_required_acks(RequiredAcks::One)
+        .create()?;
+
+    println!("Sending msg: {:?}", data);
+    producer.send(&Record {
+        topic,
+        partition: -1,
+        key: (),
+        value: data,
+    })?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use rdev::{listen, Event, EventType};
+mod kafka_producer;
+use kafka_producer::send_to_kafka;
 
 fn handle_keyboard_events(event_type: EventType) {
     // it's safe to assume that the event will always be either KeyPress or KeyRelease event, this is handled one level up (callback fn)
     let key_context: Option<rdev::Key> = match event_type {
         EventType::KeyPress(key) => Some(key),
-        EventType::KeyRelease(key) => Some(key),
+        EventType::KeyRelease(_) => None,
         _ => None,
     };
-    println!("key context: {:?}", key_context.unwrap());
+    if let Some(key_) = key_context {
+        println!("Key context: {:?}", key_);
+        send_to_kafka(key_);
+    }
 }
 
 fn callback(event: Event) {
